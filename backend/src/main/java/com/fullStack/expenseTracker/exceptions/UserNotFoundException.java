da0e88a5a07231216e78dbde542cf271be656787
varun-mck <135826063+varun-mck@users.noreply.github.com>
2024-12-05 20:26:18 +0530

    feat: commit for GQA-8924

--- backend/src/main/java/com/fullStack/expenseTracker/exceptions/UserNotFoundException.java
+++ backend/src/main/java/com/fullStack/expenseTracker/exceptions/UserNotFoundException.java
@@ -1,9 +1,28 @@
 package com.fullStack.expenseTracker.exceptions;
 
-public class UserNotFoundException extends Exception{
+/**
+ * Custom exception thrown when a user is not found in the database.
+ * This exception is used by RoleService methods.
+ */
+public class UserNotFoundException extends RuntimeException {
+   
+    // Default constructor
+    public UserNotFoundException() {
+        super("User not found");
+    }
 
+    // Constructor with a custom error message
     public UserNotFoundException(String message) {
         super(message);
     }
 
+    // Constructor with a custom error message and a cause
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    // Constructor with a cause
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
